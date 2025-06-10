0c8009bd4e10e9ebeee53d9ff8fbe464db1a54ec
JavierGlez06 <l20140191@queretaro.tecnm.mx>
2025-06-10 12:15:50 -0600

    for of

--- 03-control-flujo/07-for-of.js
+++ 03-control-flujo/07-for-of.js
@@ -2,3 +2,14 @@
 // do while
 // for
 
+let animales = ['perro', 'gato', 'conejo', 'loro'];
+
+for (let animal of animales){
+    console.log(animal);
+}
+
+let i = 0;
+while ( i < animales.length){
+    console.log(animales[i]);
+    i++;
+}
